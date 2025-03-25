22e56753f3b8ffebdbacdf700ef423d26fdb99af
Petr Hanak <51941477+xhanak2@users.noreply.github.com>
2025-03-25 08:24:22 +0100

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testAddition() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
